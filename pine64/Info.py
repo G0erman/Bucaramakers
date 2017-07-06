5e6779d0f4b767e0e39c388244ee1232c18de00a
G0erman <goerman.rafi@gmail.com>
2017-07-06 14:03:35 -0500

    Apuntes 20170704

---
+++ pine64/Info.py
@@ -0,0 +1,35 @@
+pine642GB
+PINE A64 is Single Board Computer powered by 
+Quad-Core ARM Cortex A53 64-Bit Processor. 
+It provides PI-2 Bus, Euler Bus and many others peripheral devices interface for makers to integrate with sensors and devices. 
+Various Operating System (OS) are made available by open source community such Mainline Kernel, Linux Distro, Android, Remix OS, Windows IoT and many more to come.
+
+################################################
+OS: ubuntuMate
+
+/*   */
+Prueba:
+Youtube, se queda colgado cuando se reproduce en toda la pantalla.
+Se calienta el procesador
+
+
+### Comandos útiles linux
+
+# virtual memory statistics
+vmstat
+
+# consumo de recursos de los procesos en ejecución
+top
+
+
+################################################
+OS: Android TV 
+
+/*   */
+Prueba:
+Buena experiencia de uso.
+
+
+##############################
+# unattended hig https://askubuntu.com/questions/798989/heavy-load-by-apt-unattended-upgrade
+# Pine64 https://www.pine64.org/?page_id=1194
